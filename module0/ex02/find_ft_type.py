52f5f8cf71bda9f92d2d8e5e38bed4732c375974
maria <winterzaq1@gmail.com>
2025-11-28 12:33:25 +0100

    [refactor]: Change variable name

--- module0/ex02/find_ft_type.py
+++ module0/ex02/find_ft_type.py
@@ -2,8 +2,8 @@ def all_thing_is_obj(object: any) -> int:
     if isinstance(object, str):
         print(f'{object} is in the kitchen : {type(object)}')
     elif isinstance(object, (list, tuple, set, dict)):
-        capitalized_type_name = object.__class__.__name__.capitalize()
-        print(f'{capitalized_type_name} : {type(object)}')
+        type_name = object.__class__.__name__.capitalize()
+        print(f'{type_name} : {type(object)}')
     else:
         print("Type not found")
     return 42
