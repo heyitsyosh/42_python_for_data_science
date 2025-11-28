30ed4db13f3f6693012b741a8066bfb51a18d342
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2025-11-28 22:45:53 +0100

    [feat]: Implement ex05, program that prints statistics about inputted text

---
+++ module00/ex05/building.py
@@ -0,0 +1,33 @@
+import sys
+import string
+
+
+def get_text_from_argv(argv: list[str]) -> str:
+    """Return argv[1] if provided, otherwise prompt the user for input."""
+    if len(argv) > 2:
+        raise AssertionError("more than one argument is provided")
+    elif len(argv) == 2:
+        return argv[1]
+    print("What is the text to count?")
+    return sys.stdin.readline()
+
+
+def print_text_stats(text: str):
+    """ Prints characteristics of the text. """
+    print(f'The text contains {len(text)} characters:')
+    print(sum(1 for c in text if c.isupper()), "upper letters")
+    print(sum(1 for c in text if c.islower()), "lower letters")
+    print(sum(1 for c in text if c in string.punctuation), "punctuation marks")
+    print(sum(1 for c in text if c.isspace()), "spaces")
+    print(sum(1 for c in text if c.isdigit()), "digits")
+
+
+def main():
+    try:
+        print_text_stats(get_text_from_argv(sys.argv))
+    except AssertionError as e:
+        print(f'AssertionError: {e}')
+
+
+if __name__ == "__main__":
+    main()
