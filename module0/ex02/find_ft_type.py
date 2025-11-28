e7d0b05f780353f10b6694a327515c27edd2e2f2
maria <winterzaq1@gmail.com>
2025-11-28 12:30:24 +0100

    [feat]: Improve conditional flow

--- module0/ex02/find_ft_type.py
+++ module0/ex02/find_ft_type.py
@@ -1,14 +1,9 @@
 def all_thing_is_obj(object: any) -> int:
-    if isinstance(object, list):
-        print("List :", type(object))
-    elif isinstance(object, tuple):
-        print("Tuple :", type(object))
-    elif isinstance(object, set):
-        print("Set :", type(object))
-    elif isinstance(object, dict):
-        print("Dict :", type(object))
-    elif isinstance(object, str):
-        print(object, "is in the kitchen :", type(object))
+    if isinstance(object, str):
+        print(f'{object} is in the kitchen : {type(object)}')
+    elif isinstance(object, (list, tuple, set, dict)):
+        capitalized_type_name = object.__class__.__name__.capitalize()
+        print(f'{capitalized_type_name} : {type(object)}')
     else:
         print("Type not found")
     return 42
