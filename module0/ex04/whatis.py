39b32a1355e16cc9c8da2ddcc233f223bedb3a65
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2025-11-28 17:07:58 +0100

    [feat]: Implement ex04, use argv and AssertionErrors

---
+++ module0/ex04/whatis.py
@@ -0,0 +1,19 @@
+import sys
+
+def what_is(num) -> str:
+    return "even" if num % 2 == 0 else "odd"
+
+def main(argv):
+    if len(argv) == 1:
+        return
+    if len(argv) > 2:
+        raise AssertionError("more than one argument is provided")
+    if not argv[1].lstrip('-').isdigit():
+        raise AssertionError("argument is not an integer")
+    num = int(argv[1])
+    print(f"I'm {what_is(num)}.")
+
+try:
+    main(sys.argv)
+except AssertionError as e:
+    print(f'AssertionError: {e}')
