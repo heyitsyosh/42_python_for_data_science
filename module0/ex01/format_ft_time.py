4fec80370953580d3b2bdcb6b7f5e25aa3e6cb77
maria <winterzaq1@gmail.com>
2025-11-28 16:07:40 +0100

    [feat]: Implement ex01, intro to time and datetime libs

---
+++ module0/ex01/format_ft_time.py
@@ -0,0 +1,6 @@
+import time, datetime
+
+today = datetime.datetime.now()
+seconds = time.time()
+print(f'Seconds since January 1, 1970: {seconds:,.4f} or {seconds:.2e} in scientific notation')
+print(today.strftime("%b %d %Y"))
