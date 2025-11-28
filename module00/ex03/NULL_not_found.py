165a22aa8b8168caa1719232135452a1aa225269
maria <winterzaq1@gmail.com>
2025-11-28 20:08:48 +0100

    [feat]: Implement ex03, classifying null-like objects

---
+++ module00/ex03/NULL_not_found.py
@@ -0,0 +1,18 @@
+def NULL_not_found(object: any) -> int:
+    if object is None:
+        name = "Nothing"
+    elif isinstance(object, float) and object != object:
+        name = "Cheese"
+    elif object is False:
+        name = "Fake"
+    elif isinstance(object, int) and object == 0:
+        name = "Zero"
+    elif object == "":
+        print(f'Empty: {type(object)}')
+        return 0
+    else:
+        print("Type not found")
+        return 1
+
+    print(f'{name}:', f'{object} {type(object)}')
+    return 0
