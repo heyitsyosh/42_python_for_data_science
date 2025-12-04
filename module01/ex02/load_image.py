28430e015d145504cd9f3b74ebaa1099fb450842
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2025-12-04 11:05:15 +0100

    [feat]: Switch image processing library to PIL in consideration of the following modules

--- module01/ex02/load_image.py
+++ module01/ex02/load_image.py
@@ -1,16 +1,15 @@
-import cv2
+from PIL import Image
 import numpy as np
 
 
 def ft_load(path: str) -> np.ndarray:
     """Loads an image and returns it as a NumPy array."""
     try:
-        if not isinstance(path, str) or not cv2.haveImageReader(path):
+        if not isinstance(path, str):
             raise ValueError(f"'{path}' is an invalid filepath")
-        img = cv2.imread(path, cv2.IMREAD_COLOR_RGB)
-        if img is None:
-            raise Exception(f'failed to read image at {path}')
-        print("The shape of the image is:", img.shape)
-        return img
+        img = Image.open(path)
+        img_arr = np.array(img)
+        print("The shape of the image is:", img_arr.shape)
+        return img_arr
     except Exception as e:
         print("Error:", e)
