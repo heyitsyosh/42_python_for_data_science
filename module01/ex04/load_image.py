fcd4fbfcb1297a41db20d42ec5a6a2fa150ebbd5
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2025-12-07 19:25:37 +0100

    [feat]: Implement ex04, function to transpose image matrix

--- module01/ex04/load_image.py
+++ module01/ex04/load_image.py
@@ -4,13 +4,10 @@ import numpy as np
 
 def ft_load(path: str) -> np.ndarray:
     """Loads an image and returns it as a NumPy array."""
-    try:
-        if not isinstance(path, str) or not cv2.haveImageReader(path):
-            raise ValueError(f"'{path}' is an invalid filepath")
-        img = cv2.imread(path, cv2.IMREAD_COLOR_RGB)
-        if img is None:
-            raise Exception('failed to read image at {path}')
-        print("The shape of the image is:", img.shape)
-        return img
-    except Exception as e:
-        print("Error:", e)
+    if not isinstance(path, str) or not cv2.haveImageReader(path):
+        raise ValueError(f"'{path}' is an invalid filepath")
+    img = cv2.imread(path, cv2.IMREAD_COLOR_RGB)
+    if img is None:
+        raise ValueError(f"failed to read image at '{path}'")
+    # print("The shape of the image is:", img.shape)
+    return img
