239ea9829ae23fc854948cc19add96510f3c7729
maria <winterzaq1@gmail.com>
2025-12-07 19:37:23 +0100

    [feat]: Add requirement.txt, testers, and assets for tests

---
+++ module01/ex05/tester.py
@@ -0,0 +1,15 @@
+from load_image import ft_load
+from pimp_image import show, ft_invert, ft_red, ft_green, ft_blue, ft_gray
+import matplotlib.pyplot as plt
+
+array = ft_load("../assets/landscape.jpg")
+
+show(array, "Original ver.")
+
+ft_invert(array)
+ft_red(array)
+ft_green(array)
+ft_blue(array)
+ft_gray(array)
+
+print(ft_invert.__doc__)
