46f7ba32a26690c14bbfc01cc7a874cd907339e9
maria <winterzaq1@gmail.com>
2025-12-12 12:38:12 +0100

    [feat]: Implement ex01, add new subclasses with __str__, __repr__, and method chaining

---
+++ module03/ex01/S1E9.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """Abstract base class named 'Character'."""
+
+    def __init__(self, first_name: str, is_alive: bool = True) -> None:
+        """Initializes a character."""
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    def die(self) -> None:
+        """Sets the is_alive parameter to false."""
+        self.is_alive = False
+
+
+class Stark(Character):
+    """'Stark' class that inherits from 'Character'."""
+
+    def __init__(self, first_name: str, is_alive: bool = True) -> None:
+        """Initializes a Stark character."""
+        super().__init__(first_name, is_alive)
