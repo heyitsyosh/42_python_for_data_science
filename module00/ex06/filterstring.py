3309d43a09ad3ac7e35db0098c6c2cc8fba1d5a4
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2025-12-15 14:58:42 +0100

    [feat]: Implement ex06, filtering items from a list using lambda function and list comprehension

---
+++ module00/ex06/filterstring.py
@@ -0,0 +1,33 @@
+import sys
+from ft_filter import ft_filter
+
+
+def validate_args(args: list[str]) -> None:
+    """Validates that arguments are given correctly.
+    String with no special characters, integer"""
+    assert len(args) == 2, f'expected 2 arguments, got {len(args)}'
+    assert all(c.isalnum() or c == ' ' for c in args[0]), \
+        "first argument contains special characters"
+    try:
+        int(args[1])
+    except ValueError:
+        assert False, f'second argument must be an integer, got {args[1]!r}'
+
+
+def filterstring(string: str, min_len: int):
+    """Returns words longer than the given minimum length"""
+    words = [word for word in string.split()]
+    return ft_filter(lambda word: len(word) > min_len, words)
+
+
+def main():
+    try:
+        validate_args(sys.argv[1:])
+        filtered = filterstring(sys.argv[1], int(sys.argv[2]))
+        print(list(filtered))
+    except AssertionError as e:
+        print("AssertionError:", e)
+
+
+if __name__ == "__main__":
+    main()
