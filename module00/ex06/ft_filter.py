c793499f668b64c35b4b5177ca8b823943bad501
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2025-12-17 15:33:01 +0100

    [refactor]: clean up

--- module00/ex06/ft_filter.py
+++ module00/ex06/ft_filter.py
@@ -1,4 +1,4 @@
-def ft_filter(function, iterable,):
+def ft_filter(function, iterable):
     """filter(function or None, iterable) --> filter object
 
 Return an iterator yielding those items of iterable for which function(item)
