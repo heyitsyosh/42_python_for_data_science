f3c85d47a3408579ea31177a6eff186a05857ce6
maria <winterzaq1@gmail.com>
2025-12-17 15:56:44 +0100

    [feat]: Add test files

---
+++ module00/ex06/tester.py
@@ -0,0 +1,10 @@
+from ft_filter import ft_filter
+
+print(filter.__doc__)
+print(ft_filter.__doc__)
+
+print(filter(None, range(3)))
+print(ft_filter(None, range(3)))
+
+print(list(filter(None, range(3))))
+print(list(ft_filter(None, range(3))))
