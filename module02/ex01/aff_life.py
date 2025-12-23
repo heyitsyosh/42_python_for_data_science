18638f16c1f1698b4b8bffa087ffe3fa5f8d8c99
maria <winterzaq1@gmail.com>
2025-12-23 17:41:55 +0100

    [feat]: Implement ex01, visualize life expectancy from csv file data

--- module02/ex01/aff_life.py
+++ module02/ex01/aff_life.py
@@ -1,8 +1,22 @@
-"""Docstring for module02.ex01.aff_life
-"""
+"""Visualize life expectancy projections for France.
+Data is extracted from a CSV containing life expectancy by country and year."""
 
 from load_csv import load
+import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+
+def visualize_france_data(df: pd.core.frame.DataFrame) -> None:
+    """"Plots France's life expectancy over time."""
+    ax = plt.subplot()
+    france_data = df.loc[df["country"] == "France"]
+    ax.plot(df.columns[1:], france_data.iloc[0, 1:])
+    ax.set(xlabel="Year",
+           ylabel='Life expectancy',
+           title='France Life Expectancy Projections')
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(40))
+    plt.show()
 
 
 def main():
@@ -10,8 +24,10 @@ def main():
     if df is None:
         return
 
-    df.loc[df["country"] == "France"]
-
+    try:
+        visualize_france_data(df)
+    except Exception as e:
+        print("Error:", e)
 
 
 if __name__ == "__main__":
