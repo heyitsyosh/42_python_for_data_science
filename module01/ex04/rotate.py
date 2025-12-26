a7ed5f831838dc55f2775a284a08dd53e2b78e51
maria <winterzaq1@gmail.com>
2025-12-27 00:09:44 +0100

    [feat]: Handle keyboard interrupts

--- module01/ex04/rotate.py
+++ module01/ex04/rotate.py
@@ -70,6 +70,8 @@ def main():
         print(transposed_img)
     except Exception as e:
         print("Error:", e)
+    except KeyboardInterrupt:
+        print("Keyboard interruption detected")
 
 
 if __name__ == "__main__":
