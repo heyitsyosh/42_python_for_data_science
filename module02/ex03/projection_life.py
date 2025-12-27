86ba0de9d40b4f51f8b5f11389011c07c5817af0
maria <winterzaq1@gmail.com>
2025-12-27 23:30:28 +0100

    [feat]: Implement ex03, display a scatter plot from csv data

--- module02/ex03/projection_life.py
+++ module02/ex03/projection_life.py
@@ -1,4 +1,37 @@
-"""
-Docstring for module02.ex03.projection_life
-"""
+"""Display a scatter plot where each point represents a country.
 
+The x-axis shows GDP per capita.
+The y-axis shows life expectancy.
+Each point is calculated using data from the year 1900."""
+
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+def main():
+    try:
+        year = "1900"
+        life_expectancy_data = load("../data/life_expectancy_years.csv")
+        gdp_data = load("../data/income_per_person_gdppercapita_"
+                        "ppp_inflation_adjusted.csv")
+
+        plt.scatter(gdp_data[year], life_expectancy_data[year])
+
+        ax = plt.subplot()
+        ax.set(
+            xlabel="Gross Domestic Product",
+            ylabel="Life Expectancy",
+            xscale="log",
+            title=year,
+            xticks=[300, 1000, 10000]
+        )
+        ax.xaxis.set_major_formatter(
+            lambda x, pos: f'{x/1000:g}k' if x >= 1000 else x
+        )
+        plt.show()
+    except Exception as e:
+        print("Error:", e)
+
+
+if __name__ == "__main__":
+    main()
