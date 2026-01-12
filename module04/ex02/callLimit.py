e6ac46a6dd3fc548917ca423ef01866d01a63259
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-01-12 12:03:15 +0100

    [feat]: Implement ex02, call-limiting decorator

--- module04/ex02/callLimit.py
+++ module04/ex02/callLimit.py
@@ -1,5 +1,19 @@
-def callLimit(limit: int):
+from typing import Any
+
+
+def callLimit(limit: int) -> object:
+    """Decorator factory."""
     count = 0
+
     def callLimiter(function):
-        def limit_function(*args: Any, **kwds: Any):
-            #your code here
+        """Decorator."""
+        def limit_function(*args: Any, **kwargs: Any):
+            """Wrapper that enforces the call limit."""
+            nonlocal count
+            count += 1
+            if count > limit:
+                print("Error:", function, "was called too many times")
+                return None
+            return function(*args, **kwargs)
+        return limit_function
+    return callLimiter
