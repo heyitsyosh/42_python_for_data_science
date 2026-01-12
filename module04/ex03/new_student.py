edf24c234fecdd4b5b94d5c37383ce0920b51745
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2026-01-12 14:23:50 +0100

    [feat]: Implement ex03, use the dataclass library to build a class

--- module04/ex03/new_student.py
+++ module04/ex03/new_student.py
@@ -2,9 +2,21 @@ import random
 import string
 from dataclasses import dataclass, field
 
-def generate_id()-> str:
-    return "".join(random.choices(string.ascii_lowercase, k = 15))
+
+def generate_id() -> str:
+    """Generates a random 15-character ID."""
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
 
 @dataclass
 class Student:
-    #your code here
+    """'Student' class. Requires name and surname parameters."""
+    name: str
+    surname: str
+    active: bool = field(default=True)
+    login: str = field(init=False)
+    id: str = field(init=False, default_factory=generate_id)
+
+    def __post_init__(self) -> None:
+        """Generates a login from the student's name and surname."""
+        self.login = (self.name[:1] + self.surname)[:8]
